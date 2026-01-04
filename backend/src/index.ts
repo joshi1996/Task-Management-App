263ab813e7c3b10c4ed34551d062e80af1e0d2ed
joshi1996 <amitbha96@gmail.com>
2026-01-05 01:35:20 +0530

    Create Task Add,Update, Delete and List apis.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import authRouter from './routes/auth.js';
+import taskRouter from './routes/task.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRouter);
+app.use("/tasks", taskRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, World and Good night !!!');
 });
 
-app.listen(8000, () => {
+app.listen(8000, "0.0.0.0", () => {
     console.log('Server is running on port 8000');
 });
